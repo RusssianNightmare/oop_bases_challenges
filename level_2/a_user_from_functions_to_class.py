02378861e7a1f2c7b0baf834bd8c9741dce19450
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-03-27 23:41:04 +0300

    Update a_user_from_functions_to_class.py

--- level_2/a_user_from_functions_to_class.py
+++ level_2/a_user_from_functions_to_class.py
@@ -4,15 +4,16 @@
 Задания:
     1. Создайте класс User и перенесите всю логику работы с пользователем туда.
 """
-
-
-def make_username_capitalized(username: str):
-    return username.capitalize()
-
-
-def generate_short_user_description(username: str, user_id: int, name: str):
-    return f'User with id {user_id} has {username} username and {name} name'
-
-
 class User:
-    pass  # код писать тут
+    def __init__(self, username: str, user_id: int, name: str):
+        self.username = username
+        self.user_id = user_id
+        self.name = name
+    def make_username_capitalized(self):
+        return self.username.capitalize()
+
+    def generate_short_user_description(self):
+        return f'User with id {self.user_id} has {self.username} username and {self.name} name'
+user = User("Maharadze", "@dsgdsag", "Artem")
+print(user.make_username_capitalized())
+print(user.generate_short_user_description())
