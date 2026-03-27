64d836e64b8d40d2d050bb7a161cd42b5ebc0f78
RusssianNightmare <123409770+RusssianNightmare@users.noreply.github.com>
2026-03-27 23:26:56 +0300

    Update d_bank_account_increase_balance.py

--- level_1/d_bank_account_increase_balance.py
+++ level_1/d_bank_account_increase_balance.py
@@ -8,15 +8,17 @@
     3. Увеличьте баланс счета у экземпляра класса с помощью метода increase_balance и снова распечатайте текущий баланс.
 """
 
-
 class BankAccount:
     def __init__(self, owner_full_name: str, balance: float):
         self.owner_full_name = owner_full_name
         self.balance = balance
-
+    def balance_bank(self):
+        return self.balance
     def increase_balance(self, income: float):
-        pass  # код писать тут
-
+        self.income = income
+        return self.balance + self.income
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    bank = BankAccount("John", 10000.54)
+    print(bank.balance_bank())
+    print(bank.increase_balance(5252.24))
